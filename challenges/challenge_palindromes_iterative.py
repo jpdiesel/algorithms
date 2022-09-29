ed4cab1f9045893cdd4b61a540caf6e13e79d5d0
jpdiesel <jpedro.diesel@gmail.com>
2022-09-29 13:59:54 -0300

    5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    if not word:
+        return False
+    return word == word[::-1]
+
+# feito com a ajuda do link a seguir:
+# https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/
