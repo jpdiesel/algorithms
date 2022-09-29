5d31eb193c361dbd46c5037586432326b47343d3
jpdiesel <jpedro.diesel@gmail.com>
2022-09-29 11:11:06 -0300

    1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    try:
+        student = int()
+        for In, Out in permanence_period:
+            if In <= target_time <= Out:
+                student += 1
+        return student
+    except (TypeError, ValueError):
+        return None
