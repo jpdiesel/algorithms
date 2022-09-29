87e08fca99d1c0f25a1056eeeadbba2d3f4ce1ba
jpdiesel <jpedro.diesel@gmail.com>
2022-09-29 13:54:19 -0300

    4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,21 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if len(nums) <= 1:
+        return False
+
+    for number in nums:
+        if type(number) is not int or number < 0:
+            return False 
+
+    seted_nums = set(nums)
+    newlist = []
+    duplist = []
+
+    if len(seted_nums) == len(nums):
+        return False
+    else:
+        for number in nums:
+            if number not in newlist:
+                newlist.append(number)
+            else:
+                duplist.append(number)
+        return duplist[0]
